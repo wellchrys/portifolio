36a666308f3d737ba1d68b97f1fd60c650c388bc
wellchrys <chrystiandejesus@hotmail.com>
2021-03-15 10:46:02 -0300

    chore: change icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Wellington Chrystian`,
+        short_name: `Well in Tech`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
